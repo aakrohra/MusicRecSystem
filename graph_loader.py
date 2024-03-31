ef4ddc43221189c5e3266ad94adf1f2f6505cb4a
ethanch1u <144387752+ethanch1u@users.noreply.github.com>
2024-03-31 00:02:03 -0400

    Add files via upload

---
+++ graph_loader.py
@@ -0,0 +1,143 @@
+"""*Insert description here*"""
+from __future__ import annotations
+import pandas
+from typing import Any
+
+
+class _Vertex:
+    """A vertex in a book review graph, used to represent a user or a book.
+
+    Each vertex item is either a user id or book title. Both are represented as strings,
+    even though we've kept the type annotation as Any to be consistent with lecture.
+
+    Instance Attributes:
+        - item: The data stored in this vertex, representing a user or book.
+        - kind: The type of this vertex: 'user' or 'book'.
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+        - self.kind in {'user', 'book'}
+    """
+    item: Any
+    kind: str
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, kind: str) -> None:
+        """Initialize a new vertex with the given item and kind.
+
+        This vertex is initialized with no neighbours.
+
+        Preconditions:
+            - kind in {'user', 'book'}
+        """
+        self.item = item
+        self.kind = kind
+        self.neighbours = set()
+
+    def degree(self) -> int:
+        """Return the degree of this vertex."""
+        return len(self.neighbours)
+
+
+class Graph:
+    """A graph used to represent a book review network.
+    """
+    # Private Instance Attributes:
+    #     - _vertices:
+    #         A collection of the vertices contained in this graph.
+    #         Maps item to _Vertex object.
+    _vertices: dict[Any, _Vertex]
+
+    def __init__(self) -> None:
+        """Initialize an empty graph (no vertices or edges)."""
+        self._vertices = {}
+
+    def add_vertex(self, item: Any, kind: str) -> None:
+        """Add a vertex with the given item and kind to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+        Do nothing if the given item is already in this graph.
+
+        Preconditions:
+            - kind in {'user', 'book'}
+        """
+        if item not in self._vertices:
+            self._vertices[item] = _Vertex(item, kind)
+
+    def add_edge(self, item1: Any, item2: Any) -> None:
+        """Add an edge between the two vertices with the given items in this graph.
+
+        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - item1 != item2
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            v2 = self._vertices[item2]
+
+            v1.neighbours.add(v2)
+            v2.neighbours.add(v1)
+        else:
+            raise ValueError
+
+    def adjacent(self, item1: Any, item2: Any) -> bool:
+        """Return whether item1 and item2 are adjacent vertices in this graph.
+
+        Return False if item1 or item2 do not appear as vertices in this graph.
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            return any(v2.item == item2 for v2 in v1.neighbours)
+        else:
+            return False
+
+    def get_neighbours(self, item: Any) -> set:
+        """Return a set of the neighbours of the given item.
+
+        Note that the *items* are returned, not the _Vertex objects themselves.
+
+        Raise a ValueError if item does not appear as a vertex in this graph.
+        """
+        if item in self._vertices:
+            v = self._vertices[item]
+            return {neighbour.item for neighbour in v.neighbours}
+        else:
+            raise ValueError
+
+    def get_all_vertices(self, kind: str = '') -> set:
+        """Return a set of all vertex items in this graph.
+
+        If kind != '', only return the items of the given vertex kind.
+
+        Preconditions:
+            - kind in {'', 'user', 'book'}
+        """
+        if kind != '':
+            return {v.item for v in self._vertices.values() if v.kind == kind}
+        else:
+            return set(self._vertices.keys())
+
+
+if __name__ == '__main__':
+
+    data = pandas.read_csv('music_genre.csv')
+    data.drop(data.columns[[3, 6, 9, 11, 12, 13, 14, 15]], axis=1, inplace=True)
+    data.dropna(inplace=True)
+    graph = Graph()
+
+    for s in data.iterrows():
+        graph.add_vertex(s[1]['instance_id'], s[1]['music_genre'])
+
+    # do we generate the edges after asking if the user wants all genres/only the same genre as the chosen song?
+
+    for s in data.iterrows():
+        data['difference'] = abs(data['acousticness'] - s[1]['acousticness'])
+        for c in data.columns[4:9]:
+            data['difference'] += abs(data[c] - s[1][c])
+        sort_by_similar = data.sort_values('difference', ascending=True)
+        for i in range(16):
+            if s[1]['instance_id'] != sort_by_similar['instance_id'][i]:
+                graph.add_edge(s[1]['instance_id'], sort_by_similar['instance_id'][i])
